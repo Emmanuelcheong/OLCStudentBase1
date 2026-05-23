41849c57fa54780db7926ba0f85f95b486770ee2
Emmanuelcheong <inamuu07@gmail.com>
2026-05-23 06:07:22 +0000

    23/5/2026
    Co-authored-by: DLEE <autom8ton@users.noreply.github.com>

---
+++ homework/hw_qn_minmax.py
@@ -0,0 +1,27 @@
+
+###############################################################
+# Scenario: Employee Performance Review
+
+# Finding Maximum, Minimum, and Average Performance Scores 
+# Without Built-in Functions
+# YOU CANNOT USE ANY PYTHON INBUILT FUNCTIONS TO DO THIS.
+
+# A company conducts annual performance reviews for employees. 
+# Each employee is given a performance score out of 100. 
+# The HR department wants to:
+
+# - Identify the top-performing employee (highest score).
+# - Identify the lowest-performing employee (lowest score).
+# - Calculate the average performance score, rounded to 2 decimal places.
+# - Identify underperforming employees (those with scores below 50) 
+#    -> save them into another dictionary called non_performers.
+#   and print a performance warning message to all of these employees.
+
+performance_scores = {
+    'Alice': 88, 'Benny': 75, 'Charlie': 92, 'David': 85,
+    'Emma': 78, 'Farah': 81, 'George': 66, 'Hassan': 94,
+    'Ivy': 71, 'Jack': 88, 'Liam': 45, 'Jessica': 98,
+    'Samir': 23, 'Jimmy': 5, 'Bryan': 78, 'Estelle': 9}
+
+# write your code here
+
